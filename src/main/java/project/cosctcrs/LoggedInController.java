2bb68ff156dd1c87c68fc667446de4473bde05f4
stephenwestmacott <stephenhwestmacott@gmail.com>
2024-03-23 02:25:43 -0400

    Fully implemented login and logout functionality, tested with several users in the DB. No bugs found so far.

---
+++ src/main/java/project/cosctcrs/LoggedInController.java
@@ -0,0 +1,39 @@
+// Controller for logged-in.fxml
+
+package project.cosctcrs;
+
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class LoggedInController implements Initializable {
+    @FXML
+    Button button_logout;
+
+    @FXML
+    Label label_title;
+
+    @FXML
+    Label label_name;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        button_logout.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                UtilityDB.changeScene(event, "log-in.fxml", "Log In", null);
+
+            }
+        });
+    }
+    public void setUserInformation(String title, String first_name, String last_name){
+        label_title.setText(title);
+        label_name.setText(first_name + " " + last_name);
+    }
+}
--- src/main/java/project/cosctcrs/UtilityDB.java
+++ src/main/java/project/cosctcrs/UtilityDB.java
@@ -1,9 +1,18 @@
 package project.cosctcrs;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.stage.Stage;
 import org.apache.commons.dbutils.DbUtils;
 
+import java.io.IOException;
 import java.sql.*;
 import java.util.ArrayList;
+import java.util.Objects;
 
 public class UtilityDB extends MyJDBC {
     public static void main(String[] args) {
@@ -92,4 +101,209 @@ public class UtilityDB extends MyJDBC {
         }
         return rows;
     }
+
+    public static void LogInUser(ActionEvent event, String username, String pass){
+        Connection connection = null;
+        PreparedStatement preparedStatement = null;
+        ResultSet resultSet = null;
+
+        try {
+            connection = DriverManager.getConnection(getURL(), getUSERNAME(), getPASSWORD());
+            preparedStatement = connection.prepareStatement("SELECT password FROM officer_logins WHERE username = ?");
+            preparedStatement.setString(1, username);
+            resultSet = preparedStatement.executeQuery();
+
+            if (!resultSet.next()) {
+                System.out.println("User not found in database.");
+                Alert alert = new Alert(Alert.AlertType.ERROR);
+                alert.setContentText("Provided credentials are incorrect");
+                alert.show();
+            } else {
+                String retrievedPassword = resultSet.getString("password");
+                if (retrievedPassword.equals(pass)){
+                    changeScene(event, "logged-in.fxml", "Database Query", username);
+                } else {
+                    System.out.println("Password is incorrect.");
+                    Alert alert = new Alert(Alert.AlertType.ERROR);
+                    alert.setContentText("Provided credentials are incorrect.");
+                }
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        } finally {
+            // Close resources in a finally block
+            try {
+                if (resultSet != null) resultSet.close();
+                if (preparedStatement != null) preparedStatement.close();
+                if (connection != null) connection.close();
+            } catch (SQLException e) {
+                // Handle or log any exceptions when closing resources
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public static void changeScene(ActionEvent event, String fxmlFile, String stage_title, String username){
+        Parent root = null;
+
+        if (username != null){
+            try {
+                FXMLLoader loader = new FXMLLoader(UtilityDB.class.getResource(fxmlFile));
+                root = loader.load();
+                LoggedInController loggedInController = loader.getController();
+                loggedInController.setUserInformation(getUserTitle(username), getUserFirstName(username), getUserLastName(username));
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        } else {
+            try {
+                root = FXMLLoader.load(Objects.requireNonNull(UtilityDB.class.getResource(fxmlFile)));
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        stage.setTitle(stage_title);
+        stage.setScene(new Scene(root,800, 500));
+        stage.show();
+    }
+
+    public static String getUserTitle(String username){
+        Connection connection = null;
+        PreparedStatement preparedStatement = null;
+        ResultSet resultSet = null;
+        int officer_id;
+
+        try {
+            connection = DriverManager.getConnection(getURL(), getUSERNAME(), getPASSWORD());
+
+            // Get officer_id to use in officer table
+            preparedStatement = connection.prepareStatement("SELECT officer_id FROM officer_logins WHERE username = ?");
+            preparedStatement.setString(1, username);
+            resultSet = preparedStatement.executeQuery();
+
+            if (!resultSet.next()) {
+                System.out.println("User not found in database.");
+                return null;
+            } else {
+                officer_id = resultSet.getInt("officer_id");
+
+                preparedStatement = connection.prepareStatement("SELECT title FROM officers WHERE officer_id = ?");
+                preparedStatement.setInt(1, officer_id);
+                resultSet = preparedStatement.executeQuery();
+
+                if (resultSet.next()) {
+                    return resultSet.getString("title");
+                } else {
+                    System.out.println("Title not found for officer_id: " + officer_id);
+                    return null;
+                }
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        } finally {
+            // Close resources in a finally block
+            try {
+                if (resultSet != null) resultSet.close();
+                if (preparedStatement != null) preparedStatement.close();
+                if (connection != null) connection.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+
+    public static String getUserFirstName(String username){
+        Connection connection = null;
+        PreparedStatement preparedStatement = null;
+        ResultSet resultSet = null;
+        int officer_id = -1; // Initialize officer_id with a default value
+
+        try {
+            connection = DriverManager.getConnection(getURL(), getUSERNAME(), getPASSWORD());
+
+            // Get officer_id to use in officer table
+            preparedStatement = connection.prepareStatement("SELECT officer_id FROM officer_logins WHERE username = ?");
+            preparedStatement.setString(1, username);
+            resultSet = preparedStatement.executeQuery();
+
+            if (!resultSet.next()) {
+                System.out.println("User not found in database.");
+                return null;
+            } else {
+                officer_id = resultSet.getInt("officer_id");
+
+                preparedStatement = connection.prepareStatement("SELECT first_name FROM officers WHERE officer_id = ?");
+                preparedStatement.setInt(1, officer_id);
+                resultSet = preparedStatement.executeQuery();
+
+                if (resultSet.next()) {
+                    return resultSet.getString("first_name");
+                } else {
+                    System.out.println("First name not found for officer_id: " + officer_id);
+                    return null;
+                }
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        } finally {
+            // Close resources in a finally block
+            try {
+                if (resultSet != null) resultSet.close();
+                if (preparedStatement != null) preparedStatement.close();
+                if (connection != null) connection.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+
+    public static String getUserLastName(String username){
+        Connection connection = null;
+        PreparedStatement preparedStatement = null;
+        ResultSet resultSet = null;
+        int officer_id = -1; // Initialize officer_id with a default value
+
+        try {
+            connection = DriverManager.getConnection(getURL(), getUSERNAME(), getPASSWORD());
+
+            // Get officer_id to use in officer table
+            preparedStatement = connection.prepareStatement("SELECT officer_id FROM officer_logins WHERE username = ?");
+            preparedStatement.setString(1, username);
+            resultSet = preparedStatement.executeQuery();
+
+            if (!resultSet.next()) {
+                System.out.println("User not found in database.");
+                return null;
+            } else {
+                officer_id = resultSet.getInt("officer_id");
+
+                preparedStatement = connection.prepareStatement("SELECT last_name FROM officers WHERE officer_id = ?");
+                preparedStatement.setInt(1, officer_id);
+                resultSet = preparedStatement.executeQuery();
+
+                if (resultSet.next()) {
+                    return resultSet.getString("last_name");
+                } else {
+                    System.out.println("Last name not found for officer_id: " + officer_id);
+                    return null;
+                }
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        } finally {
+            // Close resources in a finally block
+            try {
+                if (resultSet != null) resultSet.close();
+                if (preparedStatement != null) preparedStatement.close();
+                if (connection != null) connection.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+
 }
