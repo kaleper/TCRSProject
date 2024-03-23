9335664604b5066426243f18ff412becd487c42a
kaleper <kalenperedo3@gmail.com>
2024-03-23 16:02:03 -0600

    Create UI to issue citations
    Created fxml file 'issue-citation.fxml'. This page assumes a driver has already been selected from the LoggedIn page. Users are able to select a citation via combobox, can enter a citation amount, and can click the issue button to create the citation for a user.
    
    Currently, the UI does not resize when the window size is changed. This will be optomized in a later commit.
    
    Main currently updated to open the issue-citation.fxml file when ran.

--- src/main/java/project/cosctcrs/Main.java
+++ src/main/java/project/cosctcrs/Main.java
@@ -12,7 +12,14 @@ public class Main extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("log-in.fxml")));
+
+        // Uncomment for usual log-in
+
+        //Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("log-in.fxml")));
+
+        // TEST - to see issue-citation page
+        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("issue-citation.fxml")));
+
         stage.setTitle("Log In");
         stage.setScene(new Scene(root, 800, 500));
         stage.show();
