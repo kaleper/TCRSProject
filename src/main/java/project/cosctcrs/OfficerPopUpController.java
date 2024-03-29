1b58e6622feccf6151d0ca390838ed75b409b89c
stephenwestmacott <stephenhwestmacott@gmail.com>
2024-03-29 14:40:57 -0400

    Created the label object so it could be populated with search data. setLabel_officer_info(String string) sets the text in the label to the parameter string.

--- src/main/java/project/cosctcrs/OfficerPopUpController.java
+++ src/main/java/project/cosctcrs/OfficerPopUpController.java
@@ -4,15 +4,19 @@ package project.cosctcrs;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class OfficerPopUpController implements Initializable {
-
+    @FXML
+    Label label_officer_info;
 
     @Override
-    public void initialize(URL url, ResourceBundle resourceBundle) {
+    public void initialize(URL url, ResourceBundle resourceBundle) {}
 
+    public void setLabel_officer_info(String string){
+        label_officer_info.setText(string);
     }
 }
